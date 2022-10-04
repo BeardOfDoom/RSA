56a68d40070c0b03ea0cda98e3263884953c0ad8
BeardOfDoom <vecsi96@gmail.com>
2022-10-04 15:35:54 +0800

    Implemented multi precision zero check.

--- mp_is_zero.s
+++ mp_is_zero.s
@@ -1,15 +1,15 @@
-.MACRO  mp_cmp  input1Address,  input2Address,  size
+.MACRO  mp_is_zero  inputAddress,  size
 
     MOV X1, \size
     SUB X1, X1, #8
+
+    MOV X3, #0
 
     1:
     EOR X4, X1, #-8
     CBZ X4, 2f
 
-        LDR X2, [\input1Address,    X1]
-        LDR X3, [\input2Address,    X1]
-
+        LDR X2, [\inputAddress,    X1]
         CMP X2, X3
 
         B.NE    2f
